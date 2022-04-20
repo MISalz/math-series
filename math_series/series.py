efc97cb07e8c4e3957e3b2311bdf7bcc54152894
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-04-19 23:45:16 -0700

    math_series

--- math_series/series.py
+++ math_series/series.py
@@ -4,8 +4,15 @@ def series(num):
 
 
 def fibonacci(n):
-
+    f = [0, 1]
+    for i in range(2, n+1):
+        f.append(f[i-1]+f[1-2])
+        return f[n]
 
 
 def lucas(n):
+    return '1'
+
+
+def sum_series(n):
     return '1'
--- tests/test_math_series.py
+++ tests/test_math_series.py
@@ -1,4 +1,4 @@
-from math_series.series import series
+from math_series.series import series, fibonacci
 
 
 def test_math_series_one():
@@ -8,3 +8,13 @@ def test_math_series_one():
     expected = '1'
     # does what happened match what you expected
     assert actual == expected
+
+
+def test_fibonacci():
+    # what happened
+    actual = fibonacci(n)
+    # what did you expect to happen
+    expected = '1'
+    # does what happened match what you expected
+    assert actual == expected
+
