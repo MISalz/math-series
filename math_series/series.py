0802351d1e137d440f41147787a75a8d0f54749e
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-04-29 15:05:31 -0700

    all tests passing

--- math_series/series.py
+++ math_series/series.py
@@ -1,10 +1,7 @@
-
 def series(num):
     return '1'
 
 
-def fibonacci(n):
-    return str(n * (n-1))
 '''
 Create a function called fibonacci. The function should have one parameter n. 
 The function should return the nth value in the fibonacci series. 
@@ -12,8 +9,15 @@ You may implement the function using recursion or iteration.
 If you are feeling particularly frisky, do both as separate functions.
 '''
 
-def lucas(n):
-    return str(n + (n - 1))
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
 
 '''
 In your series.py module, add a new function lucas that returns the nth value in the lucas numbers. 
@@ -21,10 +25,14 @@ Again, you may use recursion or iteration, or both.
 Again, ensure that your function has a well-formed docstring.
 '''
 
+def lucas(n):
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        return lucas(n - 2) + lucas(n - 1)
 
-
-def sum_series(n, m=0, p=1):
-    return str(n + (p - 1))
 
 '''
 Add a third function called sum_series with one required parameter and two optional parameters. 
@@ -38,3 +46,12 @@ Other values for the optional parameters will produce other series.
 Again, you may use recursion or iteration, or both. 
 Again, ensure that your function has a well-formed docstring.
 '''
+
+
+def sum_series(n, m=0, p=1):
+    if n == 0:
+        return m
+    elif n == 1:
+        return p
+    else:
+        return sum_series(n - 1, m, p) + sum_series(n - 2, m, p)
