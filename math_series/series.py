2ef2eb05940fb14e82d981005354c6f28c098f1d
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-05-02 20:41:53 -0700

    docstrings

--- math_series/series.py
+++ math_series/series.py
@@ -3,6 +3,12 @@ def series(num):
 
 
 def fibonacci(n):
+    '''
+    Create a function called fibonacci. The function should have one parameter n.
+    The function should return the nth value in the fibonacci series.
+    You may implement the function using recursion or iteration.
+    If you are feeling particularly frisky, do both as separate functions.
+    '''
     if n == 0:
         return 0
     elif n == 1:
@@ -11,15 +17,15 @@ def fibonacci(n):
         return fibonacci(n - 1) + fibonacci(n - 2)
 
 
-'''
-Create a function called fibonacci. The function should have one parameter n. 
-The function should return the nth value in the fibonacci series. 
-You may implement the function using recursion or iteration. 
-If you are feeling particularly frisky, do both as separate functions.
-'''
+
 
 
 def lucas(n):
+    '''
+    In your series.py module, add a new function lucas that returns the nth value in the lucas numbers.
+    Again, you may use recursion or iteration, or both.
+    Again, ensure that your function has a well-formed docstring.
+    '''
     if n == 0:
         return 2
     elif n == 1:
@@ -28,14 +34,22 @@ def lucas(n):
         return lucas(n - 2) + lucas(n - 1)
 
 
-'''
-In your series.py module, add a new function lucas that returns the nth value in the lucas numbers. 
-Again, you may use recursion or iteration, or both. 
-Again, ensure that your function has a well-formed docstring.
-'''
+
 
 
 def sum_series(n, m=0, p=1):
+    '''
+    Add a third function called sum_series with one required parameter and two optional parameters.
+    The required parameter will determine which element in the series to print.
+    The two optional parameters will have default values of 0 and
+    1 and will determine the first two values for the series to be produced.
+
+    Calling this function with no optional parameters will produce numbers from the fibonacci series.
+    Calling it with the optional arguments 2 and 1 will produce values from the lucas numbers.
+    Other values for the optional parameters will produce other series.
+    Again, you may use recursion or iteration, or both.
+    Again, ensure that your function has a well-formed docstring.
+    '''
     if n == 0:
         return m
     elif n == 1:
@@ -43,16 +57,3 @@ def sum_series(n, m=0, p=1):
     else:
         return sum_series(n - 1, m, p) + sum_series(n - 2, m, p)
 
-
-'''
-Add a third function called sum_series with one required parameter and two optional parameters. 
-The required parameter will determine which element in the series to print. 
-The two optional parameters will have default values of 0 and 
-1 and will determine the first two values for the series to be produced.
-
-Calling this function with no optional parameters will produce numbers from the fibonacci series. 
-Calling it with the optional arguments 2 and 1 will produce values from the lucas numbers. 
-Other values for the optional parameters will produce other series. 
-Again, you may use recursion or iteration, or both. 
-Again, ensure that your function has a well-formed docstring.
-'''
